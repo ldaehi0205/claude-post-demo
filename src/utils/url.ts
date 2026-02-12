1977e91dad3190dfdae3383ae0884555779cba95
ldaehi0205 <ldaehi0205@gmail.com>
2026-02-12 14:12:42 +0900

    디버깅 커밋

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -12,7 +12,6 @@ export function getBaseUrl(): string {
   }
 
   if (process.env.VERCEL_URL) {
-    console.log(process.env.VERCEL_URL, '---');
     return `https://${process.env.VERCEL_URL}`;
   }
 
