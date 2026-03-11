e2cfddab42a8ead1299d49a92363fa6cd4ff0c6e
ldaehi0205 <ldaehi0205@gmail.com>
2026-03-12 01:09:04 +0900

    [auto] Update: src/app/posts/_components/PostList.tsx

--- src/app/posts/_components/PostList.tsx
+++ src/app/posts/_components/PostList.tsx
@@ -95,6 +95,7 @@ export function PostList({ tag }: PostListProps) {
             <th className="py-3 px-4 w-32 text-center">작성일</th>
             <th className="py-3 px-2 w-16 text-center">조회</th>
             <th className="py-3 px-2 w-16 text-center">댓글</th>
+            <th className="py-3 px-2 w-16 text-center">좋아요</th>
           </tr>
         </thead>
         <tbody>
