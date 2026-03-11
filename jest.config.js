ffe06349c25f0566c899436ab7ebae0c6e5f7c68
ldaehi0205 <ldaehi0205@gmail.com>
2026-03-12 01:03:08 +0900

    [auto] Update: jest.config.js 외 2개 파일

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,27 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   testMatch: ['**/__tests__/**/*.test.ts'],
+  testEnvironmentOptions: {},
+  projects: [
+    {
+      displayName: 'node',
+      preset: 'ts-jest',
+      testEnvironment: 'node',
+      roots: ['<rootDir>/src'],
+      moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
+      testMatch: [
+        '**/__tests__/api/**/*.test.ts',
+        '**/__tests__/apis/**/*.test.ts',
+        '**/__tests__/utils/**/*.test.ts',
+      ],
+    },
+    {
+      displayName: 'jsdom',
+      preset: 'ts-jest',
+      testEnvironment: 'jsdom',
+      roots: ['<rootDir>/src'],
+      moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
+      testMatch: ['**/__tests__/hooks/**/*.test.ts'],
+    },
+  ],
 };
